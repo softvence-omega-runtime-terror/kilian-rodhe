ed949e909eeaed25892522d61f5c3804448def29
Abrrar <madborakash48@gmail.com>
2026-01-24 08:36:10 +0600

    remove .next

--- .next/static/chunks/app_pages_my-creation_page_tsx_97a56679._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__49ac4b02._.js",
-  "static/chunks/node_modules_b24fc148._.js",
-  "static/chunks/[next]_internal_font_google_d418629d._.css"
-],
-    source: "dynamic"
-});
