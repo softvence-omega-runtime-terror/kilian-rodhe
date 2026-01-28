72931fbc92e2d5bf35c35bb058b0c4641f18483f
robiul093 <robihasan093@gmail.com>
2026-01-28 08:36:30 +0600

    update next config file

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    unoptimized: true,
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tundra-backend-s3.s3.amazonaws.com",
+        pathname: "/**",
+      },
       {
         protocol: "https",
         hostname: "tundra-backend-s3.s3.us-east-1.amazonaws.com",
@@ -13,5 +18,4 @@ const nextConfig: NextConfig = {
     ],
   },
 };
-
 export default nextConfig;
