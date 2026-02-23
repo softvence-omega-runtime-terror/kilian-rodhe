e09343d29270ead91f8d2254719be231197c7103
Abrrar <madborakash48@gmail.com>
2026-02-23 18:42:27 +0600

    this commit for the little bit updated and fixes some errors

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   images: {
     unoptimized: true,
     remotePatterns: [
